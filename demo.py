cb721c7c6a9dcd5221a177da542c2a6f292ab472
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2024-01-04 13:41:06 +0900

    demo script

--- demo.py
+++ demo.py
@@ -92,11 +92,11 @@ def predict(m, path, tfs, cls_names):
 if __name__ == "__main__":
     
     # Initialize argument parser
-    parser = argparse.ArgumentParser(description = 'Object Classification Demo')
+    parser = argparse.ArgumentParser(description = "Sport Types Classification Demo")
     
     # Add arguments
-    parser.add_argument("-mn", "--model_name", type = str, default = 'rexnet_150', help = "Model name for backbone")
-    parser.add_argument("-cp", "--checkpoint_path", type = str, default = 'saved_models/sports_best_model_rexnet_150_pl.ckpt', help = "Path to the checkpoint")
+    parser.add_argument("-mn", "--model_name", type = str, default = "rexnet_150", help = "Model name for backbone")
+    parser.add_argument("-cp", "--checkpoint_path", type = str, default = "path/to/checkpoint/", help = "Path to the checkpoint")
     
     # Parse the arguments
     args = parser.parse_args() 
